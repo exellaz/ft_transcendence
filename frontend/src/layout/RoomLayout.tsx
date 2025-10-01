acc275968a69d98e4a78015e2cf54a6e1b8dc559
low <lowwenjuin27@gmail.com>
2025-10-01 16:11:07 +0800

    fix: leader able to change game setting and only leader able to view it

--- frontend/src/layout/RoomLayout.tsx
+++ frontend/src/layout/RoomLayout.tsx
@@ -9,25 +9,22 @@ import InviteFriendsPopup from "../popups/InviteFriendsPopup";
 
 interface RoomLayoutProps {
   children: React.ReactNode;
+  isLeader: boolean | null;
 }
 
-const RoomLayout: React.FC<RoomLayoutProps> = ({ children }) => {
+const RoomLayout: React.FC<RoomLayoutProps> = ({ children, isLeader }) => {
   const { t } = useTranslation();
   const translate = (key: string) => t(`RoomLayout.${key}`);
   const [showGameSettings, setShowGameSettings] = useState(false);
   const [showInviteFriends, setShowInviteFriends] = useState(false);
 
-  //TODO replace with JWT
-  const playerInfo = JSON.parse(sessionStorage.getItem("playerInfo") || "{}");
   const roomId = sessionStorage.getItem("RoomId");
   if (!roomId) return <div>{translate("no_room")}</div>;
-  const roomLeaderId = sessionStorage.getItem("RoomLeaderId") || "";
-
 
   return (
     <Background>
       <div className="absolute top-10 right-10 flex-col-center gap-6">
-		{roomLeaderId === playerInfo.id && (
+		{isLeader === true && (
         <Button variant="profile" onClick={() => setShowGameSettings(true)}>
           {translate("game_settings")}
         </Button>
