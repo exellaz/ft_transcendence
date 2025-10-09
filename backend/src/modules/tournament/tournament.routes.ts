b7b0cad05e431e09c444ab730356a64fa5dda011
Samuel Ting <samuelting7@gmail.com>
2025-10-09 17:14:20 +0800

    feat: added fastify endpoint function for `GET /users/:id/tournament-stats`

--- backend/src/modules/tournament/tournament.routes.ts
+++ backend/src/modules/tournament/tournament.routes.ts
@@ -1,5 +1,7 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { generateRoomId } from "../room/room";
+import { TournamentPlayer } from "@prisma/client";
+import { ok } from "src/utils/response";
 
 interface tournament {
   id: number;
@@ -64,4 +66,58 @@ export default async function tournamentRoutes(app: FastifyInstance) {
     }));
     return response;
   });
+
+  // GET /users/:id/tournament-history  - tournament history + matches
+  // app.get("/users/:id/tournament-history", async (request, response) => {
+  //   const { id } = request.params as { id: string };
+  //   const userId = Number(id);
+
+  //   const players: TournamentPlayer[] = await app.db.tournamentPlayer.findMany({
+  //     where: {
+  //       userId: userId,
+  //       tournament: { status: 'COMPLETED' },
+  //     }
+  //   });
+
+    
+    
+
+  // });
+  
+
+  // GET /users/:id/tournament-stats
+  app.get("/users/:id/tournament-stats", async (request, response) => {
+    const { id } = request.params as { id: string };
+    const userId = Number(id);
+
+    const completedTournaments: TournamentPlayer[] = await app.db.tournamentPlayer.findMany({
+      where: {
+        userId: userId,
+        tournament: { status: 'COMPLETED' },
+      },
+      select: { ranking: true },
+    });
+
+    const rankings = completedTournaments.map(t => t.ranking);
+
+    // use prisma to get average ranking
+    const stats = await app.db.tournamentPlayer.aggregate({
+      _avg: { ranking: true },
+      where: {
+        userId: userId,
+        tournament: { status: 'COMPLETED' },
+      },
+    });
+
+    const tournamentStats = {
+      firstPlace: rankings.filter(r => r === 1).length,
+      secondPlace: rankings.filter(r => r === 2).length,
+      thirdPlace: rankings.filter(r => r === 3).length,
+      completedTournaments: rankings.length,
+      averageRanking: stats._avg.ranking // if user hasn't join tournaments, avgRanking = null
+    };
+
+    return ok(tournamentStats);
+  });
+
 }
