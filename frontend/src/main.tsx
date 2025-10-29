ed3ffa6b7c18e4cab637c84fd4156c2a36473612
et-learns-to-code <155518416+et-learns-to-code@users.noreply.github.com>
2025-10-29 22:20:29 +0800

    chore: disabled React Query DevTools.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App.tsx";
 import "./style.css";
 import { OnlineStatusProvider } from "./context/OnlineStatusProvider.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/queryClient";
 
 createRoot(document.getElementById("root")!).render(
@@ -24,7 +24,7 @@ createRoot(document.getElementById("root")!).render(
       </LanguageProvider>
       {/* DevTools will provide a floating panel to see the state of the cache, 
       queries and mutations - useful for debugging */}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </StrictMode>,
 );
